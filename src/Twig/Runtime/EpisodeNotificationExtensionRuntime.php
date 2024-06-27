45e79d2fb87aa75394780e0e888786569bab154a
Yoli35 <ojm16@free.fr>
2024-06-27 11:40:38 +0200

    New episode of the day display in menu with progress for episode group
    Before:
    
    That '90s Show S02E01
    That '90s Show S02E02
    That '90s Show S02E03
    That '90s Show S02E04
    That '90s Show S02E05
    That '90s Show S02E06
    That '90s Show S02E07
    That '90s Show S02E08
    
    After:
    
    That '90s Show S02E01 à S02E08

--- src/Twig/Runtime/EpisodeNotificationExtensionRuntime.php
+++ src/Twig/Runtime/EpisodeNotificationExtensionRuntime.php
@@ -30,11 +30,81 @@ readonly class EpisodeNotificationExtensionRuntime implements RuntimeExtensionIn
 
     public function listEpisodeOfTheDay(User $user, string $country = 'FR', string $locale = 'fr'): array
     {
-        $arr = array_map(function ($item) {
-            $item['posterPath'] = '/series/posters' . $item['posterPath'];
-            return $item;
-        }, $this->userEpisodeRepository->episodesOfTheDayForTwig($user, $country, $locale));
-        dump($arr);
-        return $arr;
+        $arr = $this->userEpisodeRepository->episodesOfTheDayForTwig($user, $country, $locale);
+        $seriesArr = [];
+        $totalEpisodeCount = 0;
+        foreach ($arr as $item) {
+            if (!$this->seriesInArray($seriesArr, $item)) {
+                $item['episodes'] = [$item['episodeNumber']];
+                $item['posterPath'] = '/series/posters' . $item['posterPath'];
+                $item['episodesWatched'] = $item['watchAt'] === null ? 0 : 1;
+                $seriesArr[$item['id']] = $item;
+            } else {
+                $seriesArr[$item['id']]['episodes'][] = $item['episodeNumber'];
+                $seriesArr[$item['id']]['episodesWatched'] += $item['watchAt'] === null ? 0 : 1;
+            }
+        }
+        foreach ($seriesArr as $key => $item) {
+            $seasonNumber = $item['seasonNumber'];
+            if (count($item['episodes']) > 1) {
+                $start = $this->minNumberInArray($item['episodes']);
+                $end = $this->maxNumberInArray($item['episodes']);
+                if ($locale === 'en')
+                    $display = sprintf('S%02dE%02d to S%02dE%02d', $seasonNumber, $start, $seasonNumber, $end);
+                else
+                    $display = sprintf('S%02dE%02d à S%02dE%02d', $seasonNumber, $start, $seasonNumber, $end);
+            } else {
+                $episodeNumber = $item['episodes'][0];
+                $display = sprintf('S%02dE%02d', $seasonNumber, $episodeNumber);
+
+            }
+            $seriesArr[$key]['display'] = $item['displayName'] . ' ' . $display;
+            $seriesArr[$key]['episodeCount'] = count($item['episodes']);
+            $totalEpisodeCount += count($item['episodes']);
+        }
+
+        return [
+            'totalEpisodeCount' => $totalEpisodeCount,
+            'results' => array_map(function ($item) {
+                return [
+                    'display' => $item['display'],
+                    'episodeCount' => $item['episodeCount'],
+                    'episodesWatched' => $item['episodesWatched'],
+                    'id' => $item['id'],
+                    'name' => $item['displayName'],
+                    'posterPath' => $item['posterPath'],
+                    'progress' => 100 * $item['episodesWatched'] / $item['episodeCount'],
+                    'seasonNumber' => $item['seasonNumber'],
+                    'slug' => $item['localizedSlug'] ?? $item['slug'],
+                ];
+            }, $seriesArr),
+        ];
+    }
+
+    private function seriesInArray($seriesArr, $item): bool
+    {
+        return key_exists($item['id'], $seriesArr);
+    }
+
+    private function minNumberInArray($arr): int
+    {
+        $min = 1000;
+        foreach ($arr as $item) {
+            if ($item < $min) {
+                $min = $item;
+            }
+        }
+        return $min;
+    }
+
+    private function maxNumberInArray($arr): int
+    {
+        $max = 0;
+        foreach ($arr as $item) {
+            if ($item > $max) {
+                $max = $item;
+            }
+        }
+        return $max;
     }
 }
