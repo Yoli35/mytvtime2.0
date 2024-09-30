5c4b5f3078dccb91e4c0bdc3c3793a500faf464b
Yoli35 <ojm16@free.fr>
2024-10-01 00:00:00 +0200

    Shooting locations page: list of locations

--- src/Repository/SeriesRepository.php
+++ src/Repository/SeriesRepository.php
@@ -71,6 +71,7 @@ class SeriesRepository extends ServiceEntityRepository
         $userId = $user->getId();
         $sql = "SELECT s.`id`                                                        as id,
                        IF(sln.name IS NULL, s.name, CONCAT(sln.name, ' - ', s.name)) as name,
+                       IF(sln.slug IS NULL, s.slug, sln.slug)                        as slug,
                        s.locations                                                   as locations
                 FROM `series` s
                          INNER JOIN user_series us ON s.id = us.series_id
