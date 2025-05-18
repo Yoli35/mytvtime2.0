9feba4250fc5b104780c05862cae1eb693f334bd
Yoli35 <ojm16@free.fr>
2025-05-18 12:38:39 +0200

    DateService: parameter types

--- src/Service/DateService.php
+++ src/Service/DateService.php
@@ -11,40 +11,7 @@ use IntlDateFormatter;
 
 class DateService
 {
-    private array $days = [
-        "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche",                                    /* Français - France */
-        "Londin", "Mårdi", "Mèrkidi", "Djûdi", "Vinrdi", "Sèmedi", "Dimègne",                                       /* Picard - France */
-        "Lundi", "Mardi", "Mercrédi", "Jéeudi", "Vendrédi", "Sammedi", "Dîmmaunche",                                /* Wallon - Belgique */
-        "Léndi", "Mardi", "Mécrdi", "Jheùdi", "Vendrdi", "Sémedi", "Dimenche",                                      /* Lorrain - France */
-        "Diluns", "Dimars", "Dimècres", "Dijòus", "Divendres", "Dissabte", "Dimenge",                               /* Occitan - France */
-        "Dilun", "Dimars", "Dimèdre", "Dijòu", "Divèndre", "Dissate", "Dimenche",                                   /* Provençal - France */
-        "Luni", "Marti", "Marcuri", "Ghjovi", "Venneri", "Sabbatu", "Dumenica",                                     /* Corsu - France */
-        "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato", "Domenica",                               /* Italiano - Italie */
-        "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo",                                   /* Español - Espagne */
-        "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte", "Diumenge",                            /* Català - Espagne */
-        "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado", "Domingo",         /* Português - Portugal */
-        "Luni", "Marţi", "Miercuri", "Joi", "Vineri", "Sâmbătă", "Duminică",                                        /* Română - Roumanie */
-        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday",                               /* English - Royaume-uni */
-        "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag",                            /* Deutsch - Allemagne */
-        "Mandi", "Zischdi", "Mittwuch", "Dunnerschdi", "Fridi", "Sàmschdi", "Sunndi",                               /* Alemannisch - Allemagne */
-        "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag",                             /* Nederlands - Pays-bas */
-        "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag",                                     /* Dansk - Danemark */
-        "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag",                                      /* Svenska - Suède */
-        "Mánnudagur", "Þriðjudagur", "Miðvikudagur", "Fimmtudagur", "Föstudagur", "Laugardagur", "Sunnudagur",      /* Íslenska - Islande */
-        "Llun", "Mawrth", "Mercher", "Iau", "Gwener", "Sadwrn", "Sul",                                              /* Cymraeg - Pays de Galles */
-        "Lun", "Meurzh", "Merc'her", "Yaou", "Gwener", "Sadorn", "Sul",                                             /* Brezhoneg - Bretagne */
-        "Diluain", "Dimàirt", "Diciadaoin", "Diardaoin", "Dihaoine", "Disathairne", "Didòmhnaich",                  /* Gàidhlig - Écosse */
-        "Luan", "Máirt", "Céadaoin", "Déardaoin", "Aoine", "Satharn", "Domhnach",                                   /* Gaeilge - Irlande */
-        "Δευτέρα", "Τρίτη", "Τετάρτη", "Πέμπτη", "Παρασκευή", "Σάββατο", "Κυριακή",                                 /* Ελληνικά - Grèce */
-        "Astelehena", "Asteartea", "Asteazkena", "Osteguna", "Ostirala", "Larunbata", "Igandea",                    /* Euskara - Pays basque */
-        "Maanantai", "Tiistai", "Keskiviikko", "Torstai", "Perjantai", "Lauantai", "Sunnuntai",                     /* Suomi - Finlande */
-        "Esmaspäev", "Teisipäev", "Kolmapäev", "Neljapäev", "Reede", "Laupäev", "Pühapäev",                         /* Eesti - Estonie */
-        "Poniedziałek", "Wtórek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela",                             /* Polski - Pologne */
-        "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье",                          /* Русский - Russie */
-        "Pirmdiena", "Otrdiena", "Trešdiena", "Ceturtdiena", "Piektdiena", "Sestdiena", "Svētdiena",                /* Latviešu - Lettonie */
-        "Jumatatu", "Jumanne", "Jumatano", "Alhamisi", "Ijumaa", "Jumamosi", "Jumapili",];                          /* Kiswahili - Tanzanie */
-
-    public function newDate($dateString, $timeZone, $allDay = false): DateTime
+    public function newDate(string $dateString, string $timeZone, bool $allDay = false): DateTime
     {
         try {
             $date = new DateTime($dateString, new DateTimeZone($timeZone));
@@ -56,7 +23,7 @@ class DateService
         return $date;
     }
 
-    public function getNow($timezone, $allDay = false): DateTime
+    public function getNow(string $timezone, bool $allDay = false): DateTime
     {
         try {
             $date = new DateTime('now', new DateTimeZone($timezone));
@@ -68,7 +35,7 @@ class DateService
         return $date;
     }
 
-    public function getNowImmutable($timezone, $allDay = false): DateTimeImmutable
+    public function getNowImmutable(string $timezone, bool $allDay = false): DateTimeImmutable
     {
         try {
             $date = new DateTimeImmutable('now', new DateTimeZone($timezone));
@@ -80,7 +47,7 @@ class DateService
         return $date;
     }
 
-    public function newDateFromUTC($dateString, $allDay = false): DateTime
+    public function newDateFromUTC(string $dateString, bool $allDay = false): DateTime
     {
         try {
             $date = new DateTime($dateString, new DateTimeZone('UTC'));
@@ -104,7 +71,7 @@ class DateService
         return $date;
     }
 
-    public function newDateFromTimestamp($timestamp, $timeZone, $allDay = false): DateTime
+    public function newDateFromTimestamp(int $timestamp, string $timeZone, bool $allDay = false): DateTime
     {
         try {
             $date = new DateTime("now");
@@ -118,7 +85,7 @@ class DateService
         return $date;
     }
 
-    public function newDateImmutableFromTimestamp($timestamp, $timeZone, $allDay = false): DateTimeImmutable
+    public function newDateImmutableFromTimestamp(int $timestamp, string $timeZone, bool $allDay = false): DateTimeImmutable
     {
         try {
             $date = new DateTimeImmutable("now");
@@ -132,45 +99,31 @@ class DateService
         return $date;
     }
 
-    public function formatDate($dateSting, $timeZone, $locale): string
+    public function formatDate(string $dateSting, string $timeZone, string $locale): string
     {
         $format = datefmt_create($locale, IntlDateFormatter::SHORT, IntlDateFormatter::NONE, $timeZone, IntlDateFormatter::GREGORIAN);
         return datefmt_format($format, $dateSting);
     }
 
-    public function formatDateLong($dateSting, $timeZone, $locale): string
+    public function formatDateLong(string $dateSting, string $timeZone, string $locale): string
     {
         $timestamp = strtotime($dateSting);
         $format = datefmt_create($locale, IntlDateFormatter::FULL, IntlDateFormatter::NONE, $timeZone, IntlDateFormatter::GREGORIAN);
         return datefmt_format($format, $timestamp);
     }
 
-    public function formatDateRelativeLong($dateSting, $timeZone, $locale): string
+    public function formatDateRelativeLong(string $dateSting, string $timeZone, string $locale): string
     {
         $timestamp = strtotime($dateSting);
         $format = datefmt_create($locale, IntlDateFormatter::RELATIVE_LONG, IntlDateFormatter::SHORT, $timeZone, IntlDateFormatter::GREGORIAN);
         return datefmt_format($format, $timestamp);
     }
 
-    public function formatDateRelativeMedium($dateSting, $timeZone, $locale): string
+    public function formatDateRelativeMedium(string $dateSting, string $timeZone, string $locale): string
     {
         $timestamp = strtotime($dateSting);
         $format = datefmt_create($locale, IntlDateFormatter::RELATIVE_MEDIUM, IntlDateFormatter::NONE, $timeZone, IntlDateFormatter::GREGORIAN);
         return datefmt_format($format, $timestamp);
-    }
-
-    public function getDayNames($count): array
-    {
-        $days = [];
-        $n = count($this->days);
-        if ($count > $n / 2) $count = $n / 2;
-        for ($i = 0; $i < $count; $i++) {
-            do {
-                $day = $this->days[rand(0, $n - 1)];
-            } while (in_array($day, $days));
-            $days[] = $day;
-        }
-        return $days;
     }
 
     public function interval($interval): DateInterval|false
