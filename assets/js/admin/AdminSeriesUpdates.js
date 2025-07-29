52af70b29725057783029ce4d58aa77843166815
Yoli35 <ojm16@free.fr>
2025-07-29 22:46:17 +0200

    Enhance admin series updates with detailed change display
    Added detailed update display for series fields in the admin panel, including visual comparison for images and improved status handling. Updated backend to return field-level changes, image URLs, and more granular update statuses. Improved frontend and styles to support new update details and visual feedback.

--- assets/js/admin/AdminSeriesUpdates.js
+++ assets/js/admin/AdminSeriesUpdates.js
@@ -3,6 +3,15 @@ import {ToolTips} from "ToolTips";
 let gThis;
 
 export class AdminSeriesUpdates {
+
+    /** @typedef Update
+     * @type {Object}
+     * @property {string} field
+     * @property {string} label
+     * @property {string|null} valueBefore
+     * @property {string|null} valueAfter
+     */
+
     constructor() {
         gThis = this;
         this.toolTip = new ToolTips();
@@ -10,11 +19,13 @@ export class AdminSeriesUpdates {
         this.globs = JSON.parse(globs.textContent);
         this.ids = this.globs['ids'];
         this.api_series_update_series = this.globs['api_series_update_series'];
+        const svgs = document.querySelector("#svgs");
+        this.rightArrow = svgs.querySelector("#right-arrow");
         this.init();
     }
 
     init() {
-        console.log(gThis.ids);
+        /*console.log(gThis.ids);*/
         const updatesButton = document.querySelector("#admin-series-updates");
 
         updatesButton.addEventListener("click", () => {
@@ -22,6 +33,7 @@ export class AdminSeriesUpdates {
             gThis.newLine([]);
 
             gThis.startIndex = 0;
+            gThis.startDate = new Date();
             gThis.updates();
         });
     }
@@ -40,20 +52,25 @@ export class AdminSeriesUpdates {
             }
         ).then(async function (response) {
             const data = await response.json();
-            console.log({data});
+            /*console.log({data});*/
             if (data.status === 'success') {
+                gThis.posterUrl = data['posterUrl'];
+                gThis.backdropUrl = data['backdropUrl'];
                 data['results'].forEach(item => {
                     gThis.newLine([
-                        {'className': item['check'] ? 'checked' : 'not-checked', 'label': item['check'] ? '✓' : 'x'},
+                        {'className': gThis.kebabCase(item['check']), 'label': item['check']},
                         {'className': 'id', 'label': item['id']},
                         {'className': 'name', 'label': item['name']},
                         {'className': 'localized-name', 'label': item['localizedName']},
                         {'className': 'date', 'label': item['lastUpdate']}
                     ]);
+                    gThis.displayUpdates(item['updates']);
                 });
                 if (data['results'].length + gThis.startIndex >= count) {
                     gThis.newLine([]);
-                    gThis.newLine([{'className': 'footer', 'label': 'Updates completed...'}]);
+                    gThis.endDate = new Date();
+                    const elapsed = (gThis.endDate.getTime() - gThis.startDate.getTime()) / 1000;
+                    gThis.newLine([{'className': 'footer', 'label': 'Updates completed in ' + elapsed +  ' seconds.'}]);
                 } else {
                     gThis.startIndex += limit;
                     gThis.updates();
@@ -73,6 +90,116 @@ export class AdminSeriesUpdates {
             lineDiv.appendChild(itemDiv);
         })
         updatesDiv.appendChild(lineDiv);
-        lineDiv.scrollIntoView({ behavior: 'smooth', block: 'end' });
+        lineDiv.scrollIntoView({ behavior: 'instant', block: 'end' });
+    }
+
+    displayUpdates(updates) {
+        // items :
+        //      ['field' => 'name', 'before' => $series->getName(), 'after' => $name];
+        //      ['field' => 'overview', 'before' => $series->getOverview(), 'after' => $overview];
+        //      ['field' => 'backdrop_path', 'before' => $series->getBackdropPath(), 'after' => $backdropPath];
+        //      ['field' => 'poster_path', 'before' => $series->getPosterPath(), 'after' => $posterPath];
+        //      ['field' => 'status', 'before' => $series->getStatus(), 'after' => $status];
+        //      ['field' => 'number of seasons', 'before' => $series->getNumberOfSeason(), 'after' => $seasonNUmber];
+        //      ['field' => 'number of episodes', 'before' => $series->getNumberOfEpisode(), 'after' => $episodeNumber];
+        if (updates.length === 0) {
+            return;
+        }
+        const posterUrl = gThis.posterUrl;
+        const backdropUrl = gThis.backdropUrl;
+        const updatesDiv = document.querySelector(".admin__series__updates");
+        const detailsDiv = document.createElement('div');
+        detailsDiv.classList.add("admin__series__updates__details");
+
+        /** @type Update item */
+        updates.forEach((item) => {
+            const detailDiv = document.createElement('div');
+            detailDiv.classList.add("admin__series__updates__detail");
+            const className = gThis.kebabCase(item.field);
+            const fieldDiv = document.createElement("div");
+            fieldDiv.classList.add("admin__update__field");
+            fieldDiv.classList.add(className);
+            const beforeDiv = document.createElement("div");
+            beforeDiv.classList.add("admin__update__before");
+            beforeDiv.classList.add(className);
+            const rightArrowSvg = gThis.rightArrow.cloneNode(true);
+            rightArrowSvg.removeAttribute("id");
+            rightArrowSvg.classList.add("admin__update__arrow");
+            const afterDiv = document.createElement("div");
+            afterDiv.classList.add("admin__update__after");
+            afterDiv.classList.add(className);
+            switch (item['field']) {
+                case 'first_air_date':
+                case 'name':
+                case 'overview':
+                case 'status':
+                case 'number':
+                case 'number_of_seasons':
+                case 'number_of_episodes':
+                    fieldDiv.innerText = item.label;
+                    beforeDiv.innerText = item.valueBefore;
+                    afterDiv.innerText = item.valueAfter;
+                    break;
+                case 'poster_path':
+                    fieldDiv.innerText = item.label;
+                    let posterPath = item.valueBefore
+                    beforeDiv.classList.add("poster");
+                    if (posterPath) {
+                        const img = document.createElement("img");
+                        img.src = posterUrl + posterPath;
+                        beforeDiv.appendChild(img);
+                    } else {
+                        beforeDiv.innerText = 'No poster';
+                    }
+
+                    posterPath = item.valueAfter;
+                    afterDiv.classList.add("poster");
+                    if (posterPath) {
+                        const img = document.createElement("img");
+                        img.src = posterUrl + posterPath;
+                        afterDiv.appendChild(img);
+                    } else {
+                        afterDiv.innerText = 'No poster';
+                    }
+                    break;
+
+                case 'backdrop_path':
+                    fieldDiv.innerText = item.label;
+                    let backdropPath = item.valueBefore
+                    beforeDiv.classList.add("backdrop");
+                    if (backdropPath) {
+                        const img = document.createElement("img");
+                        img.src = backdropUrl + backdropPath;
+                        beforeDiv.appendChild(img);
+                    } else {
+                        beforeDiv.innerText = 'No poster';
+                    }
+
+                    backdropPath = item.valueAfter;
+                    afterDiv.classList.add("backdrop");
+                    if (backdropPath) {
+                        const img = document.createElement("img");
+                        img.src = backdropUrl + backdropPath;
+                        afterDiv.appendChild(img);
+                    } else {
+                        afterDiv.innerText = 'No backdrop';
+                    }
+                    break;
+            }
+            detailDiv.append(fieldDiv);
+            detailDiv.appendChild(beforeDiv);
+            detailDiv.appendChild(rightArrowSvg);
+            detailDiv.appendChild(afterDiv);
+            detailsDiv.appendChild(detailDiv);
+        });
+
+        updatesDiv.appendChild(detailsDiv);
+    }
+
+    kebabCase(string) {
+        return string
+            .replace(/([a-z])([A-Z])/g, "$1-$2")
+            .replace(/[\s_]+/g, '-')
+            .toLowerCase();
     }
 }
--- src/Api/SeriesUpdates.php
+++ src/Api/SeriesUpdates.php
@@ -2,20 +2,24 @@
 
 namespace App\Api;
 
-use App\Entity\Series;
 use App\Repository\SeriesRepository;
 use App\Service\DateService;
+use App\Service\ImageConfiguration;
+use App\Service\TMDBService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\String\Slugger\AsciiSlugger;
 
 #[Route('/api/series', name: 'api_series_')]
 class SeriesUpdates extends AbstractController
 {
     public function __construct(
-        private readonly DateService      $dateService,
-        private readonly SeriesRepository $seriesRepository,
+        private readonly DateService        $dateService,
+        private readonly ImageConfiguration $imageConfiguration,
+        private readonly SeriesRepository   $seriesRepository,
+        private readonly TMDBService        $tmdbService,
     )
     {
         // Inject dependencies if needed
@@ -26,41 +30,146 @@ class SeriesUpdates extends AbstractController
     {
         $data = json_decode($request->getContent(), true);
         $ids = $data['ids'] ?? [];
-        $lastId = -1;
-        $status = 'success';
-        $message = '';
         $results = [];
-        $now = $this->dateService->getNow('Europe/Paris', true);
+        $endedSeriesStatus = ['Ended', 'Canceled'];
+        $slugger = new AsciiSlugger();
 
         foreach ($ids as $item) {
+            $now = $this->dateService->getNowImmutable('UTC');
+            dump($now->format('Y-m-d H:i:s'));
+            $updates = [];
             $series = $this->seriesRepository->find($item['id']);
             if ($series) {
+                $name = $series->getName() ?: 'Unknown Series';
                 $lastUpdate = $series->getUpdatedAt();
-                $check = false;
+                $checkStatus = 'Checking';
                 $diff = $now->diff($lastUpdate);
-                if ($diff->days > 1) {
-                    $check = true; // More than 1 day since last update
+
+                if ($diff->days < 1) { // More than 1 day since last update
+                    $checkStatus = 'Passed';
                 }
+                if ($series->getStatus() && in_array($series->getStatus(), $endedSeriesStatus)) {
+                    $checkStatus = 'Ended';
+                }
+
+                if ($checkStatus !== 'Checking') {
+                    $results[] = [
+                        'check' => $checkStatus,
+                        'id' => $series->getId(),
+                        'name' => $name,
+                        'localizedName' => $series->getLocalizedName('fr')?->getName() ?? '',
+                        'updates' => [],
+                        'lastUpdate' => ucfirst($this->dateService->formatDateRelativeLong($lastUpdate->format('Y-m-d H:i:s'), "Europe/Paris", "fr")),
+                    ];
+                    continue;
+                }
+
+                $tvFR = json_decode($this->tmdbService->getTv($item['tmdb_id'], 'fr-FR'), true);
+                $tvUS = json_decode($this->tmdbService->getTv($item['tmdb_id'], 'en-US'), true);
+
+                if (!$tvFR || !$tvUS) {
+                    $checkStatus = 'Not Found';
+                    $name .= ' - Series not found on TMDB (' . $item['tmdb_id'] . ')';
+                    $this->addFlash('error', 'Series not found on TMDB: ' . $name);
+
+                    $results[] = [
+                        'check' => $checkStatus,
+                        'id' => $item['id'],
+                        'name' => $name,
+                        'localizedName' => '',
+                        'updates' => [],
+                        'lastUpdate' => ucfirst($this->dateService->formatDateRelativeLong($lastUpdate->format('Y-m-d H:i:s'), "Europe/Paris", "fr")),
+                    ];
+                    continue;
+                }
+
+                $firstAirDate = $tvFR['first_air_date'] ?? $tvUS['first_air_date'] ?? null;
+                $dbFirstAirDate = $series->getFirstAirDate()?->format("Y-m-d");
+                if ($firstAirDate && $dbFirstAirDate !== $firstAirDate) {
+                    dump(['tmdb' => $firstAirDate, 'db' => $dbFirstAirDate]);
+                    $updates[] = ['field' => 'first_air_date', 'label' => 'First air date', 'valueBefore' => $series->getFirstAirDate() ? $series->getFirstAirDate()->format('Y-m-d') : 'No date', 'valueAfter' => $firstAirDate];
+                    $series->setFirstAirDate($this->dateService->newDateImmutable($firstAirDate, 'Europe/Paris', true));
+                }
+
+                $name = $tvFR['name'] ?? $tvUS['name'] ?? null;
+                if ($name && $series->getName() !== $name) {
+                    $updates[] = ['field' => 'name', 'label' => 'Name', 'valueBefore' => $series->getName(), 'valueAfter' => $name];
+                    $series->setName($name);
+                    $series->setSlug($slugger->slug($name)->lower()->toString());
+                }
+
+                $overview = $tvFR['overview'] ?? $tvUS['overview'] ?? null;
+                if ($overview && !$series->getOverview()) {
+                    $updates[] = ['field' => 'overview', 'label' => 'Overview', 'valueBefore' => $series->getOverview(), 'valueAfter' => $overview];
+                    $series->setOverview($overview);
+                }
+
+                $backdropPath = $tvFR['backdrop_path'] ?? $tvUS['backdrop_path'] ?? null;
+                if ($backdropPath && $series->getBackdropPath() !== $backdropPath) {
+                    $updates[] = ['field' => 'backdrop_path', 'label' => 'Backdrop', 'valueBefore' => $series->getBackdropPath(), 'valueAfter' => $backdropPath];
+                    $series->setBackdropPath($backdropPath);
+                }
+
+                $posterPath = $tvFR['poster_path'] ?? $tvUS['poster_path'] ?? null;
+                if ($posterPath && $series->getPosterPath() !== $posterPath) {
+                    $updates[] = ['field' => 'poster_path', 'label' => 'Poster', 'valueBefore' => $series->getPosterPath(), 'valueAfter' => $posterPath];
+                    $series->setPosterPath($posterPath);
+                }
+
+                $status = $tvFR['status'] ?? $tvUS['status'] ?? null;
+                if ($status && $series->getStatus() !== $status) {
+                    $updates[] = ['field' => 'status', 'label' => 'Status', 'valueBefore' => $series->getStatus(), 'valueAfter' => $status];
+                    $series->setStatus($status);
+                }
+
+                $seasonNUmber = $tvFR['number_of_seasons'] ?? $tvUS['number_of_seasons'] ?? null;
+                if ($seasonNUmber && $series->getNumberOfSeason() !== $seasonNUmber) {
+                    $updates[] = ['field' => 'number_of_seasons', 'label' => 'Number of seasons', 'valueBefore' => $series->getNumberOfSeason(), 'valueAfter' => $seasonNUmber];
+                    $series->setNumberOfSeason($seasonNUmber);
+                }
+
+                $episodeNumber = $tvFR['number_of_episodes'] ?? $tvUS['number_of_episodes'] ?? null;
+                if ($episodeNumber && $series->getNumberOfEpisode() !== $episodeNumber) {
+                    $updates[] = ['field' => 'number_of_episodes', 'label' => 'Number of episodes', 'valueBefore' => $series->getNumberOfEpisode(), 'valueAfter' => $episodeNumber];
+                    $series->setNumberOfEpisode($episodeNumber);
+                }
+
+                $series->setUpdatedAt($now);
+                $this->seriesRepository->save($series, true);
+                $lastUpdate = $now;
+
+                if (count($updates)) {
+                    $checkStatus = 'Updated';
+                } else {
+                    $checkStatus = 'No changes';
+                }
+
                 $results[] = [
-                    'check' => $check,
+                    'check' => $checkStatus,
                     'id' => $series->getId(),
-                    'name' => $series->getName(),
+                    'name' => $name,
                     'localizedName' => $series->getLocalizedName('fr')?->getName() ?? '',
-                    'lastUpdate' => ucfirst($this->dateService->formatDateRelativeLong($lastUpdate->format('Y-m-d H:i:s'), "Europe/Paris", "fr")),
+                    'updates' => $updates,
+                    'lastUpdate' => ucfirst($this->dateService->formatDateRelativeLong($lastUpdate->format('Y-m-d H:i:s'), "Europe/Paris", "fr") . '(' . $diff->days . ' days ago)'),
                 ];
-                $lastId = max($lastId, $series->getId());
             } else {
-                $status = 'error';
-                $message = 'Series not found. Id: ' . $item['id'];
-                break; // Stop processing if any series is not found
+
+                $results[] = [
+                    'check' => 'Not found',
+                    'id' => $item['id'],
+                    'name' => 'Unknown in database',
+                    'localizedName' => '',
+                    'updates' => [],
+                    'lastUpdate' => ucfirst($this->dateService->formatDateRelativeLong($now->format('Y-m-d H:i:s'), "Europe/Paris", "fr")),
+                ];
             }
         }
 
         return new JsonResponse([
-            'lastId' => $lastId,
-            'message' => $message,
-            'status' => $status,
+            'status' => 'success',
             'results' => $results,
+            'posterUrl' => $this->imageConfiguration->getUrl('poster_sizes', 5),
+            'backdropUrl' => $this->imageConfiguration->getUrl('backdrop_sizes', 3),
         ]);
     }
 }
