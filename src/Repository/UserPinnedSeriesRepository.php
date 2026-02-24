234082e60c7880835bc7894e659634592b1ab29f
Yoli35 <ojm16@free.fr>
2026-02-24 10:32:43 +0100

    Use parameterized query in pinned series retrieval
    Improves security and reliability of pinned series queries by switching to parameterized queries. Prevents potential SQL injection risks and enhances query handling within the database.

--- src/Repository/UserPinnedSeriesRepository.php
+++ src/Repository/UserPinnedSeriesRepository.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use App\Entity\UserPinnedSeries;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\DBAL\Exception;
+use Doctrine\DBAL\ParameterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -39,30 +40,40 @@ class UserPinnedSeriesRepository extends ServiceEntityRepository
 
     public function getPinnedSeriesByUser(User $user, string $locale): array
     {
-        $userId = $user->getId();
-        $sql = "SELECT s.`id`           as id,
-                       s.`poster_path`  as posterPath,
-                       ups.`created_at` as createdAt,
-                       (IF(sln.`name` IS NULL, s.`name`, sln.`name`)) as name,
-                       (IF(sln.`name` IS NULL, s.`slug`, sln.`slug`)) as slug,
-                       swl.`name` as linkName,
-                       wp.`logo_path` as providerLogoPath,
-                       wp.`provider_name` as providerName
-                FROM `user_pinned_series` ups
-                    INNER JOIN `user_series` us ON ups.`user_id` =$userId AND ups.`user_series_id` = us.`id`
-                    INNER JOIN `series` s ON us.`series_id` = s.`id`
-                    LEFT JOIN series_localized_name sln ON s.id = sln.series_id AND sln.locale = '$locale'
-                    LEFT JOIN `series_watch_link` swl ON swl.`series_id`=s.`id`
-                    LEFT JOIN `watch_provider` wp ON wp.`provider_id`=swl.`provider_id` 
-                ORDER BY ups.`created_at` DESC";
 
-        return $this->getAll($sql);
+        $params = [
+            'userId' => $user->getId(),
+            'locale' => $locale,
+        ];
+        $types = [
+            'userId' => ParameterType::INTEGER,
+            'locale' => ParameterType::STRING,
+        ];
+        $sql = <<<SQL
+            SELECT s.`id`                                             AS id,
+                       s.`poster_path`                                AS posterPath,
+                       ups.`created_at`                               AS createdAt,
+                       (IF(sln.`name` IS NULL, s.`name`, sln.`name`)) AS name,
+                       (IF(sln.`name` IS NULL, s.`slug`, sln.`slug`)) AS slug,
+                       swl.`name`                                     AS linkName,
+                       wp.`logo_path`                                 AS providerLogoPath,
+                       wp.`provider_name`                             AS providerName
+            FROM `user_pinned_series` ups
+                INNER JOIN `user_series` us ON ups.`user_id` = :userId AND ups.`user_series_id` = us.`id`
+                INNER JOIN `series` s ON us.`series_id` = s.`id`
+                LEFT JOIN series_localized_name sln ON s.id = sln.series_id AND sln.locale = :locale
+                LEFT JOIN `series_watch_link` swl ON swl.`series_id`=s.`id`
+                LEFT JOIN `watch_provider` wp ON wp.`provider_id`=swl.`provider_id` 
+            ORDER BY ups.`created_at` DESC
+        SQL;
+
+        return $this->getAll($sql, $params, $types);
     }
 
-    public function getAll($sql): array
+    public function getAll(string $sql, array $params = [], array $types = []): array
     {
         try {
-            return $this->em->getConnection()->fetchAllAssociative($sql);
+            return $this->em->getConnection()->fetchAllAssociative($sql, $params, $types);
         } catch (Exception) {
             return [];
         }
