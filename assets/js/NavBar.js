a808ff02af53b4e517dac890a1f7f06fcfa3ae54
Yoli35 <ojm16@free.fr>
2026-03-17 21:20:16 +0100

    Remove countdown timer on expiration
    Removes the countdown timer element when it expires. Reduces visual clutter and enhances user experience by removing non-functional elements.

--- assets/js/NavBar.js
+++ assets/js/NavBar.js
@@ -45,7 +45,7 @@ export class NavBar {
                 const timeDifference = initialDate - currentDate;
                 if (timeDifference < 0) {
                     clearInterval(countDownInterval);
-                    countDownDiv.innerText = "00:00:00:00";
+                    countDownDiv.remove();
                     return;
                 }
                 const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
