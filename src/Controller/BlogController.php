3ae2c8ce0a6d11caf4904895c7e0c17b9e150284
Yoli35 <ojm16@free.fr>
2026-05-11 19:09:41 +0200

    Add edit, save, and delete functionality for blog articles
    Introduces options to edit, save, and delete blog articles, providing users with greater control over content management. Improves interaction with articles by adding translation support, confirmation dialogues, and a polished UI for these actions.

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[Route('/{_locale}/blog', name: 'app_blog_', requirements: ['locale' => 'fr|en|ko'])]
 final class BlogController extends AbstractController
 {
     public function __construct(
         private readonly ArticleRepository $articleRepository,
+        private readonly TranslatorInterface $translator,
     )
     {
     }
@@ -27,12 +31,42 @@ final class BlogController extends AbstractController
     }
 
     #[Route('/article/{id}-{slug}', name: 'article', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'], methods: ['GET'])]
-    public function show(int $id): Response
+    public function show(Article $article): Response
     {
-        $article = $this->articleRepository->find($id);
-
         return $this->render('blog/show.html.twig', [
             'article' => $article,
         ]);
+    }
+
+    #[Route('/article/edit/{id}', name: 'article_edit', requirements: ['id' => '\d+'], methods: ['GET'])]
+    public function edit(Article $article): Response
+    {
+        return $this->render('blog/edit.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/article/save/{id}', name: 'article_save', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function save(Request $request, Article $article): Response
+    {
+        $payload = $request->request->all();
+        dump($payload);
+        $article->setTitle($payload['title']);
+        $article->setAuthor($payload['author']);
+        $article->setContent($payload['content']);
+
+        $this->articleRepository->save($article, true);
+        $this->addFlash('success', $this->translator->trans('Article saved successfully.'));
+
+        return $this->json(['ok' => true]);
+    }
+
+    #[Route('/article/delete/{id}', name: 'article_delete', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function delete(Article $article): Response
+    {
+        $this->articleRepository->remove($article, true);
+        $this->addFlash('success', $this->translator->trans('Article deleted successfully.'));
+
+        return $this->redirectToRoute('app_blog_index');
     }
 }
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -11,8 +12,24 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class ArticleRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
+    public function __construct(ManagerRegistry $registry, private readonly EntityManagerInterface $em)
     {
         parent::__construct($registry, Article::class);
+    }
+
+    public function save(Article $article, bool $flush = false): void
+    {
+        $this->em->persist($article);
+        if ($flush) {
+            $this->em->flush();
+        }
+    }
+
+    public function remove(Article $article, bool $flush = false): void
+    {
+        $this->em->remove($article);
+        if ($flush) {
+            $this->em->flush();
+        }
     }
 }
